799b00045f613c023364fcead16db15e0ffc6c5d
Eli <109231330+theLadEli@users.noreply.github.com>
2024-02-24 16:23:13 +0000

    Built multi-step form

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
 var activeQuestion = 1;
+var progressWidth = 168.67;
 
-$(document).ready(function (){
+var userSelection = {
+    extension: "",
+    size: "",
+    finish: "",
+}
+
+$(document).ready(function () {
     console.log("jQuery successfully loaded.")
 })
 
 function nextQuestion() {
-    console.log("Function called.")
+    console.log("nextQuestion function called.")
 
-    $(".question-1").css("display", "none");
+    $("#question-" + activeQuestion).removeClass("active");
+    activeQuestion++;
+    $("#question-" + activeQuestion).addClass("active");
+
+    // Adjust Progress Bar
+    progressWidth = progressWidth + 150;
+    $(".progress").css("width", progressWidth)
+
 
 }
+
+function previousQuestion() {
+    console.log("previousQuestion function called.")
+    $("#question-" + activeQuestion).removeClass("active");
+    activeQuestion--;
+    $("#question-" + activeQuestion).addClass("active");
+
+    // Adjust Progress Bar
+    progressWidth = progressWidth - 150;
+    $(".progress").css("width", progressWidth)
+
+}
+
+$('#extension-options input[type="radio"').on("click", function () {
+    userSelection.extension = this.value;
+    console.log(userSelection)
+})
+
+$('#size-options input[type="radio"').on("click", function () {
+    userSelection.size = this.value;
+    console.log(userSelection)
+})
+
+$('#finish-options input[type="radio"').on("click", function () {
+    userSelection.finish = this.value;
+    console.log(userSelection)
+})
