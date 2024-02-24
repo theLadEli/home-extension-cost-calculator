9b9d0858bb790f5f4ad34f061a092994df36d2f6
Eli <109231330+theLadEli@users.noreply.github.com>
2024-02-24 14:23:56 +0000

    Started JS

---
+++ script.js
@@ -0,0 +1,12 @@
+var activeQuestion = 1;
+
+$(document).ready(function (){
+    console.log("jQuery successfully loaded.")
+})
+
+function nextQuestion() {
+    console.log("Function called.")
+
+    $(".question-1").css("display", "none");
+
+}
